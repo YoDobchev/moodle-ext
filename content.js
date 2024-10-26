334c531b8059c514ec60e43a4fa7e51cbbff489d
yo <yo.dobchev@gmail.com>
2024-10-26 18:38:53 +0300

    some files do not load with the webpage

--- content.js
+++ content.js
@@ -1,9 +1,18 @@
 document.querySelectorAll("a").forEach((a) => {
     if (a.href === "https://learn.fmi.uni-sofia.bg/login/index.php") {
         window.location.href = "https://learn.fmi.uni-sofia.bg/login/index.php";
-        return;
     }
     if (a.href.endsWith("?forcedownload=1")) {
         a.href = a.href.replace("?forcedownload=1", '');
+    }
+});
+
+document.addEventListener("click", (ev) => {
+    if (ev.target && ev.target.nodeName === 'A') {
+        document.querySelectorAll("a").forEach((a) => {
+            if (a.href.endsWith("?forcedownload=1")) {
+                a.href = a.href.replace("?forcedownload=1", '');
+            }
+        });
     }
 });
