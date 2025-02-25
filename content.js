bad26cd105941bd56ab5719727e5845d2bd8abb1
YoDobchev <yo.dobchev@gmail.com>
2025-02-26 00:06:54 +0200

    Add support for elearn.bg

--- content.js
+++ content.js
@@ -1,7 +1,15 @@
+supportedMoodles = [
+    "https://learn.fmi.uni-sofia.bg",
+    "https://elearn.uni-sofia.bg"
+];
+
 document.querySelectorAll("a").forEach((a) => {
-    if (a.href === "https://learn.fmi.uni-sofia.bg/login/index.php") {
-        window.location.href = "https://learn.fmi.uni-sofia.bg/login/index.php";
+    for (let moodle of supportedMoodles) {
+        if (a.href === `${moodle}/login/index.php`) {
+            window.location.href = `${moodle}/login/index.php`;
+        }
     }
+
     if (a.href.endsWith("?forcedownload=1")) {
         a.href = a.href.replace("?forcedownload=1", '');
     }
